02315e015fc7c412646ff49bce5068fa402dd219
Umeshan <umeshanuc@gmail.com>
2023-03-27 01:45:04 +0530

    icon+BaseURL

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 from backend import create_app
 
 
-sslc = "/etc/letsencrypt/live/whonear.xyz/fullchain.pem"
-sslck = "/etc/letsencrypt/live/whonear.xyz/privkey.pem"
+# sslc = "/etc/letsencrypt/live/whonear.xyz/fullchain.pem"
+# sslck = "/etc/letsencrypt/live/whonear.xyz/privkey.pem"
 
 
 # if __name__ == "__main__":
